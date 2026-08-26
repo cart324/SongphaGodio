84ba3ac32941b3652a3c4df288838207c3d44ad6
cart324 <cart324@users.noreply.github.com>
2026-08-27 04:08:06 +0900

    add 'back_to_lobby2' command, compress duplicate codes

--- cogs/WF_song.py
+++ cogs/WF_song.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from functools import wraps
 import traceback
 from modules.error_notifier import send_error_log
 
@@ -13,114 +14,68 @@ async def download_song(message_id: int, bot: discord.Bot) -> str:
     return song_url
 
 
+def song_command(source):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(self, ctx):
+            try:
+                if isinstance(source, int):
+                    link = await download_song(source, self.bot)
+                else:
+                    link = source
+
+                audio_player_cog = self.bot.get_cog('audio_player')
+                await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
+            except Exception:
+                await send_error_log(traceback.format_exc(), ctx.author.name)
+
+                if ctx.response.is_done():
+                    await ctx.followup.send('알 수 없는 오류입니다.', ephemeral=True)
+                else:
+                    await ctx.respond('알 수 없는 오류입니다.', ephemeral=True)
+
+        return commands.slash_command(
+            guild_ids=[312795500757909506, 1242846739434569738]
+        )(wrapper)
+
+    return decorator
+
+
 class WFSong(commands.Cog, name="wf_song"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(guild_ids=[312795500757909506, 1242846739434569738])
+    @song_command(1337422572161335368)
     async def back_to_lobby(self, ctx):
         """Nacas의 불후의 명곡 'back to lobby'를 재생합니다."""
-        try:
-            link = await download_song(1337422572161335368, self.bot)
-            audio_player_cog = self.bot.get_cog("audio_player")
-            await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
-        except Exception:
-            await send_error_log(traceback.format_exc(), ctx.author.name)
-            
-            if ctx.response.is_done():
-                await ctx.followup.send("알 수 없는 오류입니다.", ephemeral=True)
-            else:
-                await ctx.respond("알 수 없는 오류입니다.", ephemeral=True)
 
-    @commands.slash_command(guild_ids=[312795500757909506, 1242846739434569738])
+    @song_command(1542231117522927767)
+    async def back_to_lobby2(self, ctx):
+        """Nacas의 최신곡 'Back to Lobby 2'를 재생합니다."""
+
+    @song_command(1337422577886302273)
     async def rage(self, ctx):
         """아니피1이야왜시발나무뒤에쳐숨어서쉴배나빨고있냐고나가서좀잡아시발요한아좀잡아"""
-        try:
-            link = await download_song(1337422577886302273, self.bot)
-            audio_player_cog = self.bot.get_cog("audio_player")
-            await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
-        except Exception:
-            await send_error_log(traceback.format_exc(), ctx.author.name)
-            
-            if ctx.response.is_done():
-                await ctx.followup.send("알 수 없는 오류입니다.", ephemeral=True)
-            else:
-                await ctx.respond("알 수 없는 오류입니다.", ephemeral=True)
 
-    @commands.slash_command(guild_ids=[312795500757909506, 1242846739434569738])
+    @song_command(1337434532303081513)
     async def rage2(self, ctx):
         """아니준서왜컨버터인데왜아직도딜량이꼴등이야아오제발(제발)"""
-        try:
-            link = await download_song(1337434532303081513, self.bot)
-            audio_player_cog = self.bot.get_cog("audio_player")
-            await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
-        except Exception:
-            await send_error_log(traceback.format_exc(), ctx.author.name)
-            
-            if ctx.response.is_done():
-                await ctx.followup.send("알 수 없는 오류입니다.", ephemeral=True)
-            else:
-                await ctx.respond("알 수 없는 오류입니다.", ephemeral=True)
 
-    @commands.slash_command(guild_ids=[312795500757909506, 1242846739434569738])
+    @song_command(1337422585482444954)
     async def great_emperor_boobs(self, ctx):
         """:clown:"""
-        try:
-            link = await download_song(1337422585482444954, self.bot)
-            audio_player_cog = self.bot.get_cog("audio_player")
-            await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
-        except Exception:
-            await send_error_log(traceback.format_exc(), ctx.author.name)
-            
-            if ctx.response.is_done():
-                await ctx.followup.send("알 수 없는 오류입니다.", ephemeral=True)
-            else:
-                await ctx.respond("알 수 없는 오류입니다.", ephemeral=True)
 
-    @commands.slash_command(guild_ids=[312795500757909506, 1242846739434569738])
+    @song_command("https://www.youtube.com/watch?v=mINlmbJcP-M")
     async def box_now_box_for_hard(self, ctx):
         """🤌🤡🤌"""
-        try:
-            link = "https://www.youtube.com/watch?v=mINlmbJcP-M"
-            audio_player_cog = self.bot.get_cog("audio_player")
-            await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
-        except Exception:
-            await send_error_log(traceback.format_exc(), ctx.author.name)
-            
-            if ctx.response.is_done():
-                await ctx.followup.send("알 수 없는 오류입니다.", ephemeral=True)
-            else:
-                await ctx.respond("알 수 없는 오류입니다.", ephemeral=True)
 
-    @commands.slash_command(guild_ids=[312795500757909506, 1242846739434569738])
+    @song_command("https://www.youtube.com/watch?v=Vxf41Pf3Ij0")
     async def waiting_for_player(self, ctx):
         """waiting for ya-ho-han wa cao ni ma sha bi"""
-        try:
-            link = "https://www.youtube.com/watch?v=Vxf41Pf3Ij0"
-            audio_player_cog = self.bot.get_cog("audio_player")
-            await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
-        except Exception:
-            await send_error_log(traceback.format_exc(), ctx.author.name)
-            
-            if ctx.response.is_done():
-                await ctx.followup.send("알 수 없는 오류입니다.", ephemeral=True)
-            else:
-                await ctx.respond("알 수 없는 오류입니다.", ephemeral=True)
 
-    @commands.slash_command(guild_ids=[312795500757909506, 1242846739434569738])
+    @song_command("https://www.youtube.com/watch?v=ezXluhqaqfI")
     async def i_sold_rice_shower(self, ctx):
         """ooh! ooh! ohh! 米! 米! 米!"""
-        try:
-            link = "https://www.youtube.com/watch?v=ezXluhqaqfI"
-            audio_player_cog = self.bot.get_cog("audio_player")
-            await audio_player_cog.play.callback(audio_player_cog, ctx, url=link)
-        except Exception:
-            await send_error_log(traceback.format_exc(), ctx.author.name)
-            
-            if ctx.response.is_done():
-                await ctx.followup.send("알 수 없는 오류입니다.", ephemeral=True)
-            else:
-                await ctx.respond("알 수 없는 오류입니다.", ephemeral=True)
 
 
 def setup(bot):
